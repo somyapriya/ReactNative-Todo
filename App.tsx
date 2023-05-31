00650e65d9d140910cf8c115c5e76fe3da92fd2a
Somya Priya <34838498+somyapriya@users.noreply.github.com>
2023-05-31 16:58:48 +0530

    Update App.tsx
    removed unwanted code

--- App.tsx
+++ App.tsx
@@ -1,22 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import LinearGradient from 'react-native-linear-gradient';
 
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
